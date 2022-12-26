643e5d5ecf3febfefce0b0929b0f9502518f8c53
Ko Kimura <vilizilesatilu@gmail.com>
2022-12-27 07:20:52 +0900

    units.js

--- data/activate.js
+++ data/activate.js
@@ -1,6 +1,8 @@
 let xlsx = require('xlsx');
 const fs = require('fs');
 const r = require("./rows.js");
+const u = require("./units.js");
+const g = require("./group.js")
 let utils = xlsx.utils;
 
 const PRIMARY = "食品番号";
@@ -132,69 +134,7 @@ let food_list ={};
 
 {
     const rows = r.nutrients_rows;
-    const units =[
-        "",
-        "",
-        "",
-        "",
-        "%",
-        "kJ",
-        "kcal",
-        "g",
-        "g",
-        "g",
-        "g",
-        "mg",
-        "g",
-        "g",
-        "",
-        "g",
-        "g",
-        "",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "μg",
-        "μg",
-        "μg",
-        "μg",
-        "μg",
-        "μg",
-        "μg",
-        "μg",
-        "μg",
-        "μg",
-        "μg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "μg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "μg",
-        "μg",
-        "mg",
-        "μg",
-        "mg",
-        "g",
-        "g",
-        "",
-    ]
+    const units = u.nutrients_units;
 
     let list = toList("data/栄養素.xlsx", rows, units, 12, utils.decode_col("BI"));
     write2JSON(list, "nutrients");
@@ -203,41 +143,7 @@ let food_list ={};
 
 {
     const rows = r.amino_acid_rows;
-
-    const units =[
-        "",
-        "",
-        "",
-        "",
-        "g",
-        "g",
-        "g",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "",
-    ]
+    const units =u.amino_acid_units;
     let list = toList("data/アミノ酸.xlsx", rows, units, 7, utils.decode_col("AF"));
     write2JSON(list, "amino_acid");
     add2List(food_list, list);
@@ -245,71 +151,7 @@ let food_list ={};
 
 {
     const rows = r.fatty_acid_rows;
-    const units =[
-        "",
-        "",
-        "",
-        "",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "",
-    ]
+    const units =u.fatty_acid_units;
     let list = toList("data/脂肪酸.xlsx", rows, units, 7, utils.decode_col("BK"));
     write2JSON(list, "fatty_acid");
     add2List(food_list, list);
@@ -317,26 +159,7 @@ let food_list ={};
 
 {
     const rows = r.carbohydrate_rows; 
-    const units =[
-        "",
-        "",
-        "",
-        "",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "",
-    ]
+    const units = u.carbohydrate_units;
     let list = toList("data/炭水化物.xlsx", rows, units, 8, utils.decode_col("R"));
     write2JSON(list, "carbohydrate");
     add2List(food_list, list);
@@ -344,22 +167,7 @@ let food_list ={};
 
 {
     const rows = r.fiber_rows;
-    const units =[
-        "",
-        "",
-        "",
-        "",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "",
-    ]
+    const units =u.fiber_units;
     let list = toList("data/食物繊維.xlsx", rows, units, 10, utils.decode_col("N"));
     write2JSON(list, "fiber");
     add2List(food_list, list);
@@ -367,37 +175,7 @@ let food_list ={};
 
 {
     const rows = r.organic_acid_rows;
-    const units =[
-        "",
-        "",
-        "",
-        "",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        "mg",
-        "mg",
-        "mg",
-        "mg",
-        "g",
-        "g",
-        "g",
-        "g",
-        "g",
-        ""
-    ]
+    const units =u.organic_acid_units;
     let list =toList("data/有機酸.xlsx", rows, units, 8, utils.decode_col("AC"));
     write2JSON(list, "organic_acid");
     add2List(food_list, list);
@@ -407,62 +185,7 @@ try {
     fs.mkdirSync(`./jsondata/group`, {recursive:true})
     for(let i in food_list){
         let obj ={data : food_list[i], id:i};
-        switch(i){
-        case "01":
-            obj.name = "穀類";
-            break;
-        case "02":
-            obj.name = "いも及びでん粉類";
-            break;
-        case "03":
-            obj.name = "砂糖及び甘味類";
-            break;
-        case "04":
-            obj.name = "豆類";
-            break;
-        case "05":
-            obj.name = "種実類";
-            break;
-        case "06":
-            obj.name = "野菜類";
-            break; 
-        case "07":
-            obj.name = "果実類";
-            break;
-        case "08":
-            obj.name = "きのこ類";
-            break;
-        case "09":
-            obj.name = "藻類";
-            break;
-        case "10":
-            obj.name = "魚介類";
-            break;
-        case "11":
-            obj.name = "肉類";
-            break;
-        case "12":
-            obj.name = "鶏卵";
-            break; 
-        case "13":
-            obj.name = "乳類";
-            break;
-        case "14":
-            obj.name = "油脂類";
-            break;
-        case "15":
-            obj.name = "菓子類";
-            break;
-        case "16":
-            obj.name = "し好飲料";
-            break;
-        case "17":
-            obj.name = "調味料及び香辛料";
-            break;
-        case "18":
-            obj.name = "調味及び流通食品類";
-            break; 
-        }
+        const group = g.group_name[i];
         fs.writeFileSync(`./jsondata/group/${i}.json`, JSON.stringify(obj).toString(), 'utf8');
     }
 }catch(err){
