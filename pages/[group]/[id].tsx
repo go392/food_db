9ee34f1679d3479144b2c865eeafc085a578f1d7
Ko Kimura <vilizilesatilu@gmail.com>
2023-01-06 21:22:04 +0900

    table tabs

--- pages/[group]/[id].tsx
+++ pages/[group]/[id].tsx
@@ -1,15 +1,11 @@
 import { InferGetStaticPropsType, GetStaticPropsContext, NextPage } from 'next'
-import Link from "next/link";
 import { FoodTable,  FoodData, FoodGroup } from "../../utils/data";
 import { table_name } from "../../data/table";
 import { BreadcrumbsGroup } from '../[group]';
-import BreadcrumbsList, { BreadcrumbsElement } from '../../components/breadcrumbslist';
+import { BreadcrumbsElement } from '../../components/breadcrumbslist';
+import * as FoodTablePage from './[id]/[slug]';
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
-type LinkInfo={
-  name:string,
-  href:string,
-}
 
 
 export const getStaticPaths = async () => { 
@@ -23,27 +19,10 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async ({params}: GetStaticPropsContext) => {
   if(params == undefined){
-    return {props: {links:[], id:"", name:"", groupname:""}};
+    return FoodTablePage.getStaticProps({ params });
   }
-  const info = FoodGroup.fromGroup(params.group as string);
-  const groupname =info.name;
-  const id = `${params.group}${params.id}`;
-  const name = info.data[id].name;
-  let paths = FoodTable.getList(id);
-  let links:LinkInfo[] = [];
-  paths.forEach((p:string) =>{
-    let href = `/${params.group}/${params.id}/${p}`;
-    let name= table_name[p as keyof typeof table_name] as string;
-    links.push({name, href});
-  })
-  return {
-    props: {
-      links,
-      id,
-      name,
-      groupname
-    }
-  }
+  let table = FoodTable.getList(`${params.group}${params.id}`)[0];
+  return FoodTablePage.getStaticProps({ params:{ group:params.group, id:params.id, slug:table} });
 }
 
 export const BreadcrumbsID= (groupname:string, id:string, foodname:string): BreadcrumbsElement[] =>{
@@ -51,12 +30,7 @@ export const BreadcrumbsID= (groupname:string, id:string, foodname:string): Brea
 }
 
 const Page: NextPage<Props> = (props : Props) => {
-  return <div className="max-w-lg m-auto">
-    <h1 className='text-2xl font-bold'>{props.name}</h1>
-    <BreadcrumbsList list={BreadcrumbsID(props.groupname, props.id, props.name)} />
-    {props.links.map((l:LinkInfo)=>
-      <Link key={l.href} className="block border border-gray-100 px-2 py-2 w-full" href={l.href}>{l.name}</Link>)}
-  </div>;
+  return <FoodTablePage.default groupname={props.groupname} id={props.id} name={props.name} data={props.data} unit={props.unit} amino_acid_score={props.amino_acid_score}  table={props.table} tableList={props.tableList}/>;
 };
 
 export default Page;
--- pages/[group]/[id]/[slug].tsx
+++ pages/[group]/[id]/[slug].tsx
@@ -5,14 +5,15 @@ import BreadcrumbsList, { BreadcrumbsElement } from '../../../components/breadcr
 import { BreadcrumbsID } from '../[id]';
 import { table_name } from '../../../data/table';
 import { calcAminoAcidScore } from '../../../constants/amino_acid';
+import Link from 'next/link';
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
 export const getStaticPaths = async () => {
-  let paths = FoodTable.getAllList();
+  const paths = FoodTable.getAllList();
   let ids: any[]=[];
   paths.forEach((v:string) => {
-    let tables = FoodTable.getList(v);
+    const tables = FoodTable.getList(v);
     tables.forEach((t:string) => {
       ids.push({params:{group:v.substring(0,2), id:v.substring(2), slug:t}});
     });
@@ -27,7 +28,7 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async ({params}: GetStaticPropsContext) => {
   if(params == undefined){
-    return {props:{data:{}, unit:{}, name:"", id:"", groupname:"", table:""}};
+    return {props:{data:{}, unit:{}, name:"", id:"", amino_acid_score:{}, groupname:"", table:"", tableList:[]}};
   }
   const info =  FoodGroup.fromGroup(params.group as string);
   const groupname =info.name;
@@ -36,6 +37,7 @@ export const getStaticProps = async ({params}: GetStaticPropsContext) => {
   const data = FoodTable.get(`${params.group}${params.id}`, `${params.slug}`) as FoodTable;
   const unit = FoodTable.get("unit", `${params.slug}`) as FoodTable;
   const amino_acid_score:Record<string, FoodTable>={};
+  const tableList = FoodTable.getList(`${params.group}${params.id}`);
   if(params.slug == "amino_acid"){
     let a = calcAminoAcidScore(FoodData.fromFoodTable(data, "amino_acid"), "2007")?.data["amino_acid"];
     if(a != undefined){
@@ -64,6 +66,7 @@ export const getStaticProps = async ({params}: GetStaticPropsContext) => {
       unit,
       amino_acid_score,
       table:params.slug as string,
+      tableList,
     }
   }
 }
@@ -86,39 +89,48 @@ function getValueString(gram:number|undefined, prop:FoodValue, unit?:FoodValue) 
   return (prop.number * gram / 100.0).toFixed(2).toString() + unitstring;
 }
 
-export const BreadcrumbsTable = (groupname:string, id:string, foodname:string, table:string): BreadcrumbsElement[] =>{
-  return [ ...BreadcrumbsID(groupname, id, foodname), {href:`/${id.substring(0,2)}/${id.substring(2)}/${table}`, show: table_name[table as keyof typeof table_name]} ];
+export const BreadcrumbsTable = (groupname:string, id:string, foodname:string): BreadcrumbsElement[] =>{
+  return BreadcrumbsID(groupname, id, foodname);
 }
 
-const Page: NextPage<Props> = (props: Props) => {
+const FoodTablePage: NextPage<Props> = (props: Props) => {
   const [gram, setGram] = useState(100);
 
   return <div className="max-w-lg m-auto">
     <h1 className='text-2xl font-bold  py-2'>{props.name}</h1>
-    <BreadcrumbsList list={BreadcrumbsTable(props.groupname, props.id, props.name, props.table)}/>
-    <input value={gram} type="number" onChange={(e)=>setGram(parseFloat(e.target.value))} className=" border bg-gray-100 w-full px-2 py-2" />
+    <BreadcrumbsList list={BreadcrumbsTable(props.groupname, props.id, props.name)}/>
+    <div className='flex'>
+    <input value={gram} type="number" onChange={(e)=>setGram(parseFloat(e.target.value))} className=" border bg-gray-100 flex-1 px-2 py-2 my-2" />
+    <div className='flex-0 px-2 py-2'>g</div>
+    </div>
+    <div className='flex w-full'>
+      {props.tableList.map((e, i)=>
+      <Link key={i} className={'text-sm inline-block rounded px-2 py-2 mx-0.5 flex-1 text-center' + (e == props.table ? " text-white bg-gray-500": " bg-gray-300 hover:bg-gray-200")} href={`/${props.id.substring(0,2)}/${props.id.substring(2)}/${e}`}>{table_name[e as keyof typeof table_name]}
+      </Link>)
+      }
+    </div>
     <table className="table-auto border-collapse border w-full">
       <tbody>{
-        Object.entries(props.data).map(([key, value]: [string, any])=><tr key={key}>
+        Object.entries(props.data).map(([key, value]: [string, any], i)=><tr key={i}>
           <th className="border bg-gray-100">{key}</th>
           <td className="border px-2 py-2">{getValueString(gram, value as FoodValue, props.unit[key as keyof typeof props.unit])}</td>
         </tr>)
       }</tbody>
     </table>
-    {Object.entries(props.amino_acid_score as Record<string, FoodTable>).map(([k, v])=>
-      <>
+    {Object.entries(props.amino_acid_score).map(([k, v], i)=>
+      <div key={i}>
       <h2 className='text-xl font-bold py-2'>{k}</h2>
       <table className="table-auto border-collapse border w-full">
         <tbody>{
-          Object.entries(v).map(([key, value]: [string, any])=><tr key={key}>
+          Object.entries(v).map(([key, value]: [string, any], i2)=><tr key={i2}>
             <th className="border bg-gray-100">{key}</th>
             <td className="border px-2 py-2">{getValueString(undefined, value as FoodValue,  props.unit[key as keyof typeof props.unit])}</td>
           </tr>)
         }</tbody>
       </table>
-      </>) 
+      </div>) 
     }
 </div>
 };
 
-export default Page;
+export default FoodTablePage;
